aea5d8c1c845840b0fbc44e269457460041f21a8
Dargothur <alex_fred2010@hotmail.com>
2026-01-12 18:42:54 -0600

    Added password generation testing

---
+++ passwords.h
@@ -0,0 +1,11 @@
+#ifndef PASSWORDS
+#define PASSWORDS
+
+using namespace std;
+
+vector<string> passwordGenerationSheet(int size);
+string passwordGeneration();
+
+
+
+#endif
--- test.cpp
+++ test.cpp
@@ -2,6 +2,9 @@
 #include <cassert>
 #include <iostream>
 #include "tokenizer.h"
+#include "passwords.h"
+
+using namespace std;
 
 std::string fileName = "testName.txt";
 
@@ -9,4 +12,31 @@ void test_tokenization(){
     std::vector<std::string> expected = {"Alex", "Mary"};
     assert(tokenizeName(fileName) == expected);
     std::cout << "Tokenizer is able to parse sample" << std::endl;
+}
+
+void test_password_length(){
+    bool passwordsAreCorrectLength = true;
+    string password = passwordGeneration();
+
+    if(password.size() != 9){
+        passwordsAreCorrectLength = false;
+    }
+
+    assert(passwordsAreCorrectLength);
+    cout << "Generated passwords are the correct length" << endl;
+}
+
+void test_password_composition(){
+    bool passwordIsLegal = true;
+    string password = passwordGeneration();
+
+    for(int i = 0; i < password.size(); i++){
+        char charToTest = password[i];
+        if(charToTest >= 'a' && charToTest <= 'z'){
+            passwordIsLegal = false;
+        }
+    }
+
+    assert(passwordIsLegal);
+    cout << "passwordGeneration creates Passwords with legal lowercase characters" << endl;
 }
